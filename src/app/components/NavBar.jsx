ceab3ded56a00ffe83ef7175265af0aea152b611
angyk <angelak1516@gmail.com>
2024-10-19 00:16:28 -0600

    icons

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Link from "next/link";
+import { Home01Icon, Task01Icon } from "hugeicons-react";
 
 export const NavBar = () => {
   return (
-    <nav className="text-center sm:text-left bg-gradient-to-tr from-[#508D4E] rounded-lg px-5 py-2">
-      <div className="text-black font-extrabold font-sans">
-        <Link className = "mr-5 hover:text-white" href="/">
-          Home
+    <nav className="flex justify-center sm:justify-start bg-gradient-to-tr from-[#508D4E] rounded-lg px-5 py-2">
+      <div className="text-black font-extrabold font-sans ">
+        <Link className = "mr-5 hover:text-white flex items-center" href="/">
+        <Home01Icon size={20}></Home01Icon>
+        Home
         </Link>
-        <Link className = "mx-5 hover:text-white" href="/">
+      </div>
+      <div className="text-black font-extrabold font-sans ">
+        <Link className = "flex items-center mx-5 hover:text-white" href="/">
+        <Task01Icon size={20}></Task01Icon>
             Projects
-        </Link>
-        <Link className = "mx-5 hover:text-white" href="/">
-            Contact Me
         </Link>
       </div>
     </nav>
