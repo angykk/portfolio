b6fb4daa109ab420e6237bf118524658b8a6bb7d
Angela Huang <angelaykhuang@gmail.com>
2025-02-10 22:42:18 -0500

    gif

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Home01Icon, Task01Icon } from "hugeicons-react";
 import { useEffect, useState } from "react";
 import getNowPlayingItem from "../spotify";
+import Image from 'next/image';
 
 export const NavBar = (props) => {
   const [loading, setLoading] = useState(true);
@@ -32,39 +33,46 @@ export const NavBar = (props) => {
   }, [result]);
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-gradient-to-r from-[#531942] via-[#923E4D] to-[#EF745C] rounded-t-lg px-5 py-2 z-50">
-      <section className="flex justify-between items-center">
-        <section className="flex justify-center items-center gap-4">
-          <section className="text-2xl font-extrabold font-sans">
-            <button className="hover:text-[#EF745C] flex items-center" onClick={props.scrollToTop}>
-              <Home01Icon size={25} className="mr-1" />
-              <span className="lg:inline hidden">Home</span>
-            </button>
-          </section>
-          <section className="text-2xl font-extrabold font-sans">
-            <button className="flex items-center hover:text-[#EF745C]" onClick={props.scrollToProjects}>
-              <Task01Icon size={25} className="mr-1" />
-              <span className="lg:inline hidden">Projects</span>
-            </button>
-          </section>
-        </section>
-        {!loading && isPlaying && (
-          <section className="flex items-center lg:gap-3">
-            <img
-              src={result.albumImageUrl}
-              alt={`${result.title} album art`}
-              className="rounded-full w-8 h-8 lg:w-12 lg:h-12 animate-spin-slow z-100"
-            />
-            <section className="flex flex-col hidden lg:flex">
-              <h1 className="text-lg hidden lg:block">Playing...</h1>
-              <a href={result.songUrl} target="_blank" className="font-medium hover:underline"><h1 className="text-white">{result.title}</h1></a>
-              <h1 className="text-sm text-white">{result.artist}</h1>
+    <span className="fixed bottom-0 left-0 right-0 z-50">
+      <Image
+        src="images/cat.gif"
+        width={180}
+        className="absolute right-0 -bottom-3"
+        height={180}
+      />
+      <nav className="bg-gradient-to-r from-[#531942] via-[#923E4D] to-[#EF745C] rounded-t-lg px-5 py-2">
+        <section className="flex justify-between items-center">
+          <section className="flex justify-center items-center gap-4">
+            <section className="text-2xl font-extrabold font-sans">
+              <button className="hover:text-[#EF745C] flex items-center" onClick={props.scrollToTop}>
+                <Home01Icon size={25} className="mr-1" />
+                <span className="lg:inline hidden">Home</span>
+              </button>
+            </section>
+            <section className="text-2xl font-extrabold font-sans">
+              <button className="flex items-center hover:text-[#EF745C]" onClick={props.scrollToProjects}>
+                <Task01Icon size={25} className="mr-1" />
+                <span className="lg:inline hidden">Projects</span>
+              </button>
             </section>
           </section>
-        )}
-      </section>
-
-    </nav>
+          {!loading && isPlaying && (
+            <section className="flex items-center lg:gap-3">
+              <img
+                src={result.albumImageUrl}
+                alt={`${result.title} album art`}
+                className="rounded-full w-8 h-8 lg:w-12 lg:h-12 animate-spin-slow z-100"
+              />
+              <section className="flex flex-col hidden lg:flex">
+                <h1 className="text-lg hidden lg:block">Playing...</h1>
+                <a href={result.songUrl} target="_blank" className="font-medium hover:underline"><h1 className="text-white">{result.title}</h1></a>
+                <h1 className="text-sm text-white">{result.artist}</h1>
+              </section>
+            </section>
+          )}
+        </section>
+      </nav>
+    </span>
   );
 };
 
